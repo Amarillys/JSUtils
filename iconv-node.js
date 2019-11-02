a6d15140d4eb33b5163857116a912a1423f96dd2
Maikaze <i@maikaze.win>
2019-11-02 15:17:29 +0800

    iconv-lite@Add-DefaultChar-Setting

--- iconv-node.js
+++ iconv-node.js
@@ -1,12 +1,14 @@
 /* eslint-env node */
 const fs = require('fs')
-const getFileList = require('./lib/getFileList')
+const getFileList = require('./getFileList')
 const iconvLite = require('iconv-lite')
 
 if (process.argv.length < 3) {
-  console.log(`usage: node iconv-node.js [inputPath] [srcEnconding] [dstEncoding] [outputFolder] [filterExtensions]...
+  console.log(`usage: node iconv-node.js [inputPath] [srcEnconding] [dstEncoding] [outputFolder] [defaultChar] [filterExtensions]...
     example: node iconv-node.js z:/originalText SHIFT_JIS UTF-8 . h cpp
-    . means overwrite inputFile
+            node iconv-node.js data/Script SHIFT_JIS GBK/· data/ScriptCN txt
+    . means overwrite inputFile, only available at target encoding
+      encoding@defaultChar
     you can add bom header in output with specified encoding: utf-8_bom
   `)
   return
@@ -15,7 +17,12 @@ if (process.argv.length < 3) {
 let argvIndex = 2
 const inputPath = process.argv[argvIndex++]
 const srcEnconding = process.argv[argvIndex++]
-let dstEncoding = process.argv[argvIndex++]
+let dstEncodingString = process.argv[argvIndex++].split('/')
+let dstEncoding = dstEncodingString[0]
+let defaultChar = dstEncodingString[1]
+if (defaultChar) {
+  iconvLite.defaultCharSingleByte = defaultChar
+}
 let outputPath = process.argv[argvIndex++]
 const filterExtensions = process.argv.slice(argvIndex)
 const filesToConv = getFileList(inputPath, false, filterExtensions)
@@ -41,7 +48,7 @@ filesToConv.forEach(file => {
     let outputFilePath = `${outputPath}/${file}`
 
     fs.mkdir(outputFilePath.slice(0, outputFilePath.lastIndexOf('/')), err => {
-      if (err.code !== 'EEXIST') {
+      if (err && err.code !== 'EEXIST') {
         console.log(err)
         return
       }
